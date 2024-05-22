abfed9a2e579ce86888e4bb9368d8978b3c1d49a
luren <lurenjia534@outlook.com>
2024-05-22 22:11:42 +0800

    Revert "marble: AlphaDroid Bringup"
    This reverts commit 9fd41cc3a69ec6d95ab7f61aaa23c13184fe45e5.

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -7,18 +7,9 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common AlphaDroid configuration
+# Inherit from common lineage configuration
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BUILD_PACKAGE := 3
-TARGET_LAUNCHER := 1
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_INCLUDE_MATLOG := false
-ALPHA_BUILD_TYPE := Official
-ALPHA_MAINTAINER := CHAITANYA
 
 PRODUCT_NAME := lineage_marble
 PRODUCT_DEVICE := marble
@@ -30,10 +21,10 @@ PRODUCT_SYSTEM_NAME := marble_global
 PRODUCT_SYSTEM_DEVICE := marble
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.5.0.UMRMIXM release-keys" \
+    PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.3.0.UMRMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.5.0.UMRMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.3.0.UMRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
